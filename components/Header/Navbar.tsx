4626115b606d7c0f3410c1d0f8db09643253db5e
Rajan66 <rajanmaharjan042@gmail.com>
2024-07-10 00:48:58 +0545

    feat: navbar

--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import Container from '../Container'
+import React from "react";
+import Container from "../Container";
+import Image from "next/image";
 
 const Navbar = () => {
-    return (
-        <nav className='bg-zinc-500'>
-            <Container> 
-                <div className=''>Navbar</div>
-            </Container>
-        </nav>
-    )
-}
+  return (
+    <nav className="bg-zinc-500 h-24">
+      <Container>
+          <div className="flex flex-row items-center justify-between h-full">
+            <div>
+              <p>BiteBuddy</p>
+              {/* <Image/> */}
+            </div>
+            <div className="">
+              <ol className="flex flex-row items-center justify-between gap-10">
+                <li>Home</li>
+                <li>Restaurants</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+              </ol>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-10">
+              <p>Login</p>
+              <p>Register</p>
+            </div>
+          </div>
+      </Container>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
