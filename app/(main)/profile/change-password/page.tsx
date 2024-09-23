abc85cc1cbb570ae066a0a07c5004ebb5bff5811
Rajan66 <rajanmaharjan042@gmail.com>
2024-09-23 23:53:42 +0545

    fix: deploy error fixes

--- app/(main)/profile/change-password/page.tsx
+++ app/(main)/profile/change-password/page.tsx
@@ -9,7 +9,7 @@ import { useMutation } from "@tanstack/react-query";
 import { changePassword } from "@/apicalls/users";
 import toast from "react-hot-toast";
 
-const page = (token: string) => {
+const page = (token: any) => {
     const {
         register,
         handleSubmit,
